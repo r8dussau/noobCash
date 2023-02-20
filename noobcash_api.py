5735f52c6246773f0c2bd2c8e75c98d42f886006
r8dussau <r8dussau@enib.fr>
2023-02-20 12:47:33 +0200

    Wallet Class

--- noobcash_api.py
+++ noobcash_api.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from Crypto.PublicKey import RSA
+
 
 #---------------------------------------------------------------------------------------------------------------
 
@@ -19,6 +21,27 @@ app= Flask(__name__)
 @app.route('/noobcash', methods=['GET', 'POST'])
 def user():
     return render_template("user.html")
+
+
+class Wallet:
+    def __init__(self, key_size):
+        self.key_size = key_size
+        self.key = RSA.generate(self.key_size)
+    
+    def get_public_key(self):
+        return self.key.publickey().export_key()
+    
+    def get_private_key(self):
+        return self.key.export_key()
+        
+keygen = Wallet(2048)
+public_key = keygen.get_public_key()
+private_key = keygen.get_private_key()
+print(public_key)
+print(private_key)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9103)
