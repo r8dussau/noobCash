71954c09a51b06ec7637e62ccaa6db881e0528c9
j8morin <j8morin@enib.fr>
2023-02-20 12:45:20 +0200

    class Transaction, class block, mise en forme

--- noobcash_api.py
+++ noobcash_api.py
@@ -22,3 +22,41 @@ def user():
 
 if __name__ == '__main__':
     app.run(debug=True, port=9103)
+
+#---------------------------------------------------------------------------------------------------------------
+#Block class
+class Block:
+    def __init__(self,index,timestamp,transactions,nonce,current_hash,previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.nonce = nonce
+        self.current_hash = current_hash
+        self.previous_hash = previous_hash
+        
+
+
+#Transcation class
+class Transaction_Input:
+    def __init__(self) -> None:
+        pass
+        
+class Transaction_Output:
+    def __init__(self) -> None:
+        pass
+
+class Transcaction:
+    def __init__(self, sender_address, receiver_address, amount, transaction_id,
+                 transaction_inputs, transaction_outputs, signature):
+        self.sender_address, self.receiver_address = sender_address, receiver_address
+        self.amount = amount
+        self.transaction_id = transaction_id
+        self.transaction_inputs, self.transaction_outputs = transaction_inputs, transaction_outputs
+        self.signature = signature
+
+#---------------------------------------------------------------------------------------------------------------
+# Creates a new transaction that contains all necessary fields. 
+# The field transaction_inputs is filled with the Transactions that contain all ids of UTXOs
+# required to get the amount we want to spend. 
+def create_transaction():
+    pass
