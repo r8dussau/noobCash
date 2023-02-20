aea30fe50accca12f048907c34e4e4bafa9bdadf
j8morin <j8morin@enib.fr>
2023-02-20 12:46:58 +0200

    Mise en forme

--- noobcash_api.py
+++ noobcash_api.py
@@ -1,28 +1,14 @@
 from flask import Flask, render_template
 
 #---------------------------------------------------------------------------------------------------------------
-
 #Aciver l'environnement virtuel:
-#env_noobcash\Scripts\activate
+        #env_noobcash\Scripts\activate
 
 #set up flask app for the env (need to restart after)
-#setx FLASK_APP "noobcash_api.py"
+        #setx FLASK_APP "noobcash_api.py"
 
 #Run l'app flask:
-#flask run
-
-#---------------------------------------------------------------------------------------------------------------
-
-
-app= Flask(__name__)
-
-@app.route('/noobcash', methods=['GET', 'POST'])
-def user():
-    return render_template("user.html")
-
-if __name__ == '__main__':
-    app.run(debug=True, port=9103)
-
+        #flask run
 #---------------------------------------------------------------------------------------------------------------
 #Block class
 class Block:
@@ -60,3 +46,14 @@ class Transcaction:
 # required to get the amount we want to spend. 
 def create_transaction():
     pass
+
+
+#---------------------------------------------------------------------------------------------------------------
+app= Flask(__name__)
+
+@app.route('/noobcash', methods=['GET', 'POST'])
+def user():
+    return render_template("user.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9103)
