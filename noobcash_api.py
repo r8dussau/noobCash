6abae435511bd89653f07fc9c53c6b5c25751efb
j8morin <j8morin@enib.fr>
2023-02-21 20:05:03 +0200

    test avec multiprocessing

--- noobcash_api.py
+++ noobcash_api.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from multiprocessing import Process
 
 #Pycryptodome
 #pip install pycroptodome 
@@ -21,8 +22,6 @@ import time
 
 #Run l'app flask:
         #flask run
-
-
 #---------------------------------------------------------------------------------------------------------------
 #Node class
 class Node:
@@ -33,7 +32,7 @@ class Node:
         self.ipAdress = f"{randint(0,200)}.{randint(0,200)}.{randint(0,200)}.{randint(0,10)}"
         self.ipPort = randint(0,5000)
         self.wallet = create_wallet(self.id,2048)
-        self.validateBlock = list()
+        self.validateBlocks = list()
         self.iteration = 0
         self.UTXOs = []
         
@@ -45,32 +44,20 @@ class Node:
             output0 = Transaction_Output("genesis",self.wallet.public_key,100)
             self.UTXOs.append(output0)
             genesisBlock = (Block(time.time(),genesisTransaction,1))
+
             genesisBlock.current_hash="0"*64
-            self.validateBlock.append(genesisBlock)
+            self.validateBlocks.append(genesisBlock)
             #ajouter aussi ce bloc à la blockchain
         else:
-            self.validateBlock.append(nodes[0].validateBlock[0])
+            self.validateBlocks.append(nodes[0].validateBlocks[0])
         
-        self.jcurrentBlock = Block(time.time(),[],self.validateBlock[len(self.validateBlock)-1].current_hash)
-        self.validateBlock.append(self.jcurrentBlock)
-
-        print(len(self.validateBlock))
-
-
-    def update(self):
-        if len(self.validateBlock) > 1:
-            validate_block(self.validateBlock[-1], self.validateBlock[-2])
-            
-        self.iteration += 1
+        self.jcurrentBlock = Block(time.time(),[],self.validateBlocks[len(self.validateBlocks)-1].current_hash)
+        #self.validateBlocks.append(self.jcurrentBlock)
         
-        
-
 #Block class
 class Block:
-    index = 0
     def __init__(self, timestamp, transactions, prev_hash):
-        Block.index += 1
-        self.index = Block.index
+        self.index = 0
         self.timestamp = timestamp
         self.transactions = transactions
         self.nonce = 0
@@ -157,7 +144,6 @@ def create_transaction(sender_publicKey, receiver_publicKey, amount, isGenesis=F
                 input = Transaction_Input(utxo.id)
                 balance += utxo.amount
                 transaction_inputs.append(input)
-                print('Une transaction  été détecter dans UTXO, je le balance dans inputs')
         #Outputs
         #Cas classique:
         transaction_outputs = []
@@ -165,11 +151,8 @@ def create_transaction(sender_publicKey, receiver_publicKey, amount, isGenesis=F
         receive_output = Transaction_Output("test", sender_publicKey, balance-amount)
         transaction_outputs.append(send_output)
         transaction_outputs.append(receive_output)
-
-        print("TAILLE de inputs:",len(transaction_inputs),'\n')
         for transaction_input in transaction_inputs:
             data += transaction_input.previous_outputID.encode()
-        print("TAILLE de outputs:",len(transaction_outputs))
         for transaction_output in transaction_outputs:
             data += transaction_output.transaction_id.encode()
         transaction_id = SHA256.new(data).hexdigest()
@@ -180,11 +163,9 @@ def create_transaction(sender_publicKey, receiver_publicKey, amount, isGenesis=F
 
         #Check balance
         if balance-amount >= 0:
-            print('Assez de thunes')
             newTransaction = Transaction(transaction_id, sender_publicKey, receiver_publicKey, amount, transaction_inputs, transaction_outputs)
             return newTransaction
         else:
-            print('Pas assez de thunes',balance)
             return None
 
 
@@ -205,33 +186,30 @@ def verify_signature(transaction, node):
     verifier = pss.new(key)
     try:
         verifier.verify(h, transaction.signature)
-        print ("The signature is authentic.")
+        # print ("The signature is authentic.")
         return True
     except (ValueError, TypeError):
-        print ("The signature is not authentic.")
+        # print ("The signature is not authentic.")
         return False
 
 def validate_transaction(transaction, node):
     # Check if signature is good
     if verify_signature(transaction, node):
-        print("La signature est bien vérifiée")
+        # print("La signature est bien vérifiée")
         for input in transaction.transaction_inputs:
             for utxo in node.UTXOs:
                 if input.previous_outputID != utxo.id:
-                    print("Pas valide")
+                    # print("Pas valide")
                     return False
                 else:
                     node.UTXOs.remove(utxo)
                     node.UTXOs.append(transaction.transaction_outputs[1])
-                    print('Valide')
+                    # print('Valide')
                     return True
 
 def broadcast_transaction(transaction):
     for node in nodes:
-        if len(node.jcurrentBlock.transactions)!=5:
-            node.jcurrentBlock.transactions.append(transaction)
-        else:
-            pass
+        node.jcurrentBlock.transactions.append(transaction)
             
 
 def wallet_balance(wallet):  
@@ -244,44 +222,46 @@ def wallet_balance(wallet):
             balance+=utxo.amount
     return balance
 
-def mine_block(node, difficulty, capacity):
+def mine_block(node, difficulty):
     nonce = 0
     #proof of work
     while node.jcurrentBlock.current_hash[0:difficulty] != '0'*difficulty:
         node.jcurrentBlock.nonce = nonce
-        
         data = f"{node.jcurrentBlock.timestamp}{node.jcurrentBlock.transactions}{node.jcurrentBlock.nonce}{node.jcurrentBlock.previous_hash}".encode()
-
         node.jcurrentBlock.current_hash = hashlib.sha256(data).hexdigest()
-        
         nonce += 1
+    mined_block = node.jcurrentBlock
+    return mined_block
 
-    return node.jcurrentBlock
-
-def broadcast_block(block, nodes):
+def broadcast_block(block):
     for node in nodes:
-        node.validateBlock.append(block)
-        node.update()
+        node.jcurrentBlock = block
 
-def validate_block(newBlock, prevBlock):
-    
+def validate_block(node):
+    newBlock = node.jcurrentBlock
+    prevBlock = node.validateBlocks[-1]
     data = f"{newBlock.timestamp}{newBlock.transactions}{newBlock.nonce}{newBlock.previous_hash}".encode()
     hash_result = hashlib.sha256(data).hexdigest()
     if hash_result != vars(newBlock)['current_hash']:
         return False
 
+    # print('---------------------------------')
+    # print('new:',vars(newBlock)['previous_hash'])
+    # print('pre:',vars(prevBlock)['current_hash'])
+    # print('---------------------------------')
+
     if vars(newBlock)['previous_hash'] != vars(prevBlock)['current_hash']:
         return False
     
-    blockchain.list.append(newBlock)
+    # blockchain.list.append(newBlock)
 
     return True
     
-def validate_chain(newBlock, prevBlock):
-    if validate_block(newBlock, prevBlock):
-        return True
-    else:
-        return False
+# def validate_chain(node):
+#     if validate_block(node):
+#         return True
+#     else:
+#         return False
 
 #---------------------------------------------------------------------------------------------------------------
 #Test ZOne
@@ -290,52 +270,64 @@ n = 2 #choose number of nodes with the front end
 nodes = list()
 for i in range(n): 
     nodes.append(Node())
-blockchain = Blockchain()
+blockchain = []
 
-test_transaction = create_transaction(nodes[0].wallet.public_key, nodes[1].wallet.public_key, 90)
+test_transaction = create_transaction(nodes[0].wallet.public_key, nodes[1].wallet.public_key, 10)
 if test_transaction != None:
     sign_transaction(test_transaction,nodes[0])
     verify_signature(test_transaction,nodes[0])
     isValid = validate_transaction(test_transaction,nodes[0])
-    b = wallet_balance(nodes[0].wallet)
-    print("balance:",b)
+    broadcast_transaction(test_transaction)
 
+test_transaction1 = create_transaction(nodes[0].wallet.public_key, nodes[1].wallet.public_key, 10)
+if test_transaction1 != None:
+    sign_transaction(test_transaction1,nodes[0])
+    verify_signature(test_transaction1,nodes[0])
+    isValid = validate_transaction(test_transaction1,nodes[0])
+    broadcast_transaction(test_transaction1)
 
+def node0():
+    if len(nodes[0].jcurrentBlock.transactions) == 2:
+        mined_block = mine_block(nodes[0],2)
+        #print(mined_block)
+        broadcast_block(mined_block)
+        if validate_block(nodes[0]):
+            mined_block.index = len(nodes[0].validateBlocks)
+            nodes[0].validateBlocks.append(mined_block)
+        nodes[0].jcurrentBlock = Block(time.time(),[],mined_block.current_hash)
 
+        print ('LENnode0:',len(nodes[0].validateBlocks))
+        print ('node0:',nodes[0].validateBlocks[0].current_hash)
+        print ('node0:',nodes[0].validateBlocks[1].current_hash)
 
-# for node in nodes:
-#     #Generation of the Genesis block
-#     if node.id==0:
-#         genesisBlock = Block(time.time(),[100*n],1) 
-#         genesisBlock.current_hash = '0'*64
+def node1():
+    if len(nodes[1].jcurrentBlock.transactions) == 2:
+        mined_block = mine_block(nodes[1],2)
+        #print(mined_block)
+        broadcast_block(mined_block)
+        if validate_block(nodes[1]):
+            mined_block.index = len(nodes[1].validateBlocks)
+            nodes[1].validateBlocks.append(mined_block)
+        nodes[1].jcurrentBlock = Block(time.time(),[],mined_block.current_hash)
 
-#     #simulation of the list transaction
-#     node.transaction = [0,1,2,3,4,5,6,7,8,9,10,11]
+        print ('LENnode1:',len(nodes[1].validateBlocks))
+        print ('node1:',nodes[1].validateBlocks[0].current_hash)
+        print ('node1:',nodes[1].validateBlocks[1].current_hash)
 
-# blockchain.list.append(genesisBlock)
+if __name__ == '__main__':
+    p1 = Process(target=node0)
+    p2 = Process(target=node1)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    
+# print ('node1:',len(nodes[1].validateBlocks))
 
-#broadcast_block(genesisBlock,nodes)
-
-#test mine_block
-
-block1 = mine_block(nodes[0],3,5)
-#print(vars(block1))
-broadcast_block(block1,nodes)
-
-block2 = mine_block(nodes[0],3,5)
-#print(vars(block2))
-broadcast_block(block2,nodes)
-
-# for node in nodes:
-#     for i in range(len(node.validateBlock)):
-#         print(f"{vars(node.validateBlock[i])}\n")
-
-# for i in range(len(blockchain.list)):
-#     if not validate_chain(blockchain.list[i+1],blockchain.list[i]):
-#         print("The chain is not valid")
-
-print(blockchain.list)
-
+# print("------------------------------")
+# print(vars(nodes[1].validateBlocks[0]))
+# print(vars(nodes[1].validateBlocks[1]))
+# print(vars(nodes[1].validateBlocks[2]))
 #---------------------------------------------------------------------------------------------------------------
 app= Flask(__name__)
 
